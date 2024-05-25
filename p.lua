cf596d608f879ad59cba0d72085021a3edc322d4
seek lol.lua <162717999+Drop56796@users.noreply.github.com>
2024-05-25 23:34:00 +0800

    Create p.lua

---
+++ p.lua
@@ -0,0 +1,35 @@
+local tip = Instance.new("ScreenGui")
+tip.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+tip.Name = "占位付"
+
+local Frame = Instance.new("Frame")
+Frame.Size = UDim2.new(100, 0, 200, 0)
+Frame.Position = UDim2.new(0.5, 0, 0.05, 0)
+Frame.AnchorPoint = Vector2.new(0.5, 0.5)
+Frame.BackgroundTransparency = 1
+Frame.Parent = tip
+
+local textLabel = Instance.new("TextLabel")
+textLabel.Text = "THE MAYHEMS..."
+textLabel.AnchorPoint = Vector2.new(0.5, 0.5)
+textLabel.Size = UDim2.new(100, 0, 10, 20)
+textLabel.Position = UDim2.new(0.50, 0, 0.5, 0)
+textLabel.Font = "Oswald"
+textLabel.TextColor3 = Color3.new(255, 222, 189)
+textLabel.BackgroundTransparency = 1
+textLabel.Parent = Frame
+textLabel.TextScaled = true
+textLabel.TextSize = 2000
+
+local tweenInfo = TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+local tween = game:GetService("TweenService"):Create(textLabel, tweenInfo, {TextTransparency = 0})
+tween:Play()
+
+wait(2)
+
+local tweenInfo = TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out)
+local tween = game:GetService("TweenService"):Create(textLabel, tweenInfo, {TextTransparency = 1})
+tween:Play()
+
+wait(5)
+tip:Destroy()
